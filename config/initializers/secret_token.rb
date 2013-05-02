cee8604b9a22d85cd24c7aecff09fe86474e6f73
lijc <lijc@ubuntu-server.(none)>
2013-05-02 14:27:06 +0800

    Init version of evolution.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evolution::Application.config.secret_token = 'd15337bc6d826b79432277448ee8f8744ef7573213ed12db25be266ed2d20dd6159c9429592b0ffd1b1bda972c6cb018f90881dba7ee8faedd0a281a1613650b'
---
+++ config/setup_load_paths.rb
@@ -0,0 +1,15 @@
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+  begin
+    gems_path = ENV['MY_RUBY_HOME'].split(/@/)[0].sub(/rubies/,'gems')
+    ENV['GEM_PATH'] = "#{gems_path}:#{gems_path}@global"
+    require 'rvm'
+    RVM.use_from_path! File.dirname(File.dirname(__FILE__))
+  rescue LoadError
+    # RVM is unavailable at this point.
+    raise "RVM ruby lib is currently unavailable."
+  end
+end
+                                       
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+require 'bundler/setup'
+
