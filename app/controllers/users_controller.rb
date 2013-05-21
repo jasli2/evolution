da5ba34d4c39750ff089ac62ea52fe6f9ba689c9
lijc <jasonliati@gmail.com>
2013-05-21 16:39:20 +0800

    Integrate simple_form for form builder.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,17 +44,12 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
 
     respond_to do |format|
-      if @user.save!
-        puts "user save success"
-        sign_in @user
+      if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
-        puts "user save failed"
-        #Debug:
-        @user.errors.full_messages
-        format.html { render action: "new"}
-        #format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { render 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
