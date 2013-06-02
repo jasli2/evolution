8077693819c34eb0dc56489826d8edb5899fa400
lijc <lijcdev@gmail.com>
2013-06-02 22:08:20 +0800

    fix user dashboard competency name error.

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,4 +2,12 @@ module CoursesHelper
   def course_cover_path(c, type)
     c.cover_image_url(type) || "default_cover_#{type}.png"
   end
+
+  def teacher_rate_tag(u)
+    i = u.teacher_rate.blank? ? 0 : u.teacher_rate.to_i
+    str = "<i class='iconfa-star'></i>" * i
+    str = str + "<i class='iconfa-star-empty'></i>" * (5-i)
+    str = "<span class='teacher-rate'>" + str + "</span>"
+    str.html_safe
+  end
 end
