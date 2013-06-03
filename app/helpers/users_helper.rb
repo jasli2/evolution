8f16337b8bf280cec123a27a5dde5958827dfdc9
lijc <lijcdev@gmail.com>
2013-06-03 21:06:43 +0800

    add User level display.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def user_avatar_path(u, type)
     u.avatar_url(type) || "defaut_user_avatar_#{type}.png"
   end
+
+  def user_level(u)
+    "Lv" + ( 2 + u.teacher_rate.to_i ).to_s
+  end
 end
