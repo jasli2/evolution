ebcebead517be2e72bcc4f2a1216c66af52b93e7
long li <hutuxiansheng@gmail.com>
2013-06-08 22:17:51 +0800

    fix relation feed item

--- app/jobs/feeditem_sque.rb
+++ app/jobs/feeditem_sque.rb
@@ -1,8 +1,8 @@
 class FeedItemSque
   @queue = "feed_item"
 
-  def self.perform(feed_item_id)
-	feed_item = FeedItem.find(feed_item_id)
-	feed_item.gen_feed_item
+  def self.perform(relation_id)
+	user_relation = UserRelation.find(relation_id)
+	user_relation.gen_feed_item
   end 	
 end
