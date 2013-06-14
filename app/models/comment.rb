5ea5fc98f068718b05e71eac8899d39708605bb7
long li <hutuxiansheng@gmail.com>
2013-06-14 17:09:23 +0800

    fix comment error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, :polymorphic => true
 
-  validates :user_id, :commentable_id, :commentable_typem, :content, :presence => true
+  validates :user_id, :commentable_id, :commentable_type, :content, :presence => true
   attr_accessible :content
   
 end
