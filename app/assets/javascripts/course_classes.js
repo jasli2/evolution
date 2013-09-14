3122d4c65ddeef33801c16fcddad61013ce13d4d
Bunco <bunco.hb@gmail.com>
2013-09-14 14:08:48 +0800

    empty users.js and course_classes.js code

--- app/assets/javascripts/course_classes.js
+++ app/assets/javascripts/course_classes.js
@@ -4,44 +4,5 @@
 
 
 (function() {
-    // $("#newCourse").click (function() {
-    //     console.log("newCourse click");
-    //     $.ajax({
-    //         url: "/courses.json",
-    //         type: "POST",
-    //         data: $("#new_course").serialize(),
-    //         cache: false,
-    //         async: false,
-    //         error: function (request) {
-    //             console.log("Connection error", request);            
-    //             for(var i in request.responseJSON) {
-    //                 if (undefined == $("#course_" + i).next(".help-inline").html()) {
-    //                     $("#course_" + i).after('<span class="help-inline">' + request.responseJSON[i] + '</span>');
-    //                 } else {
-    //                     $("#course_" + i).next(".help-inline").text(request.responseJSON[i]);
-    //                 }                
-    //                 $("#course_" + i).parents(".control-group").addClass("error");
-    //                 console.log(i,request.responseJSON[i], 
-    //                     $("#course_" + i).parents(".control-group").html());
-    //             }
-    //         },
-    //         success: function(data){
-    //             // var beforeEmployeeCount = $("#competency").next(".media-body").children(".media-heading").text();
-    //             // $("#competency").next(".media-body").children(".media-heading").text(++ beforeEmployeeCount);
-    //             console.log(beforeEmployeeCount, data);
-    //             var a = '<div class="alert fade in alert-info"><button class="close" data-dismiss="alert">×</button>成功创建用户 <strong>' +
-    //                 data.name + '</strong></div>';
-    //             $(".breadcrumbs").after(a);
-    //             $("#courses_form").modal("hide");
-    //         },
-    //         beforeSend: function(){
-    //             var control_groups = $(".control-group");
-    //             console.log("beforeSend ", control_groups);            
-    //             for(var i = 0; i < control_groups.length; i ++) {
-    //                 $(control_groups[i]).removeClass("error");
-    //                 $(control_groups[i]).find(".help-inline").text("");
-    //             }
-    //         }
-    //     });
-    // });
+	
 }).call(this);
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,44 +4,5 @@
 
 
 (function() {
-    // $("#newUsers").click (function() {
-    //     console.log("newUsers click");
-    //     $.ajax({
-    //         url: "/users.json",
-    //         type: "POST",
-    //         data: $("#new_user").serialize(),
-    //         cache: false,
-    //         async: false,
-    //         error: function (request) {
-    //             console.log("Connection error", request);            
-    //             for(var i in request.responseJSON) {
-    //                 if (undefined == $("#user_" + i).next(".help-inline").html()) {
-    //                     $("#user_" + i).after('<span class="help-inline">' + request.responseJSON[i] + '</span>');
-    //                 } else {
-    //                     $("#user_" + i).next(".help-inline").text(request.responseJSON[i]);
-    //                 }                
-    //                 $("#user_" + i).parents(".control-group").addClass("error");
-    //                 console.log(i,request.responseJSON[i], 
-    //                     $("#user_" + i).parents(".control-group").html());
-    //             }
-    //         },
-    //         success: function(data){
-    //             var beforeEmployeeCount = $("#competency").next(".media-body").children(".media-heading").text();
-    //             $("#competency").next(".media-body").children(".media-heading").text(++ beforeEmployeeCount);
-    //             console.log(beforeEmployeeCount, data);
-    //             var a = '<div class="alert fade in alert-info"><button class="close" data-dismiss="alert">×</button>成功创建用户 <strong>' +
-    //                 data.name + '</strong></div>';
-    //             $(".breadcrumbs").after(a);
-    //             $("#users_form").modal("hide");
-    //         },
-    //         beforeSend: function(){
-    //             var control_groups = $(".control-group");
-    //             console.log("beforeSend ", control_groups);            
-    //             for(var i = 0; i < control_groups.length; i ++) {
-    //                 $(control_groups[i]).removeClass("error");
-    //                 $(control_groups[i]).find(".help-inline").text("");
-    //             }
-    //         }
-    //     });
-    // });
+    
 }).call(this);
