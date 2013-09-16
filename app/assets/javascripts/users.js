0e0bdba81836610d340fb03dd13d5b4a1f7b084c
Bunco <bunco.hb@gmail.com>
2013-09-16 14:38:50 +0800

    modify quick button style, change login page logo to text, add header border bottom, add auto get user notification

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -4,5 +4,12 @@
 
 
 (function() {
-    
+	var i = 0;
+	var uid = $(".userloggedinfo input:hidden").val();
+    setInterval(function() {
+    	console.log("current count: ", i ++);
+    	$.get("/users/" + uid + "/notifications.json?" + i, function(data) {
+    		console.log(data);
+    	});
+    }, 1 * 1000);
 }).call(this);
